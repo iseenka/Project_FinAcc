0438061e7b86b9517c26e780dbf59b6f8849f21e
Oksana K <iseenka@oksanas-mac.local>
2017-03-10 15:12:59 +0200

    Initialization

---
+++ FinancialAccounting/FinancialAccounting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinancialAccounting
+//
+//  Created by Oksana K on 3/10/17.
+//  Copyright © 2017 Oksana K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
